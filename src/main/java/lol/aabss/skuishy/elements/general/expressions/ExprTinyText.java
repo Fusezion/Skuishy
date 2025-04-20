d91ffb59887063fd192df3b669a2348e42636ef4
Fusezion <fusezionstream@gmail.com>
2025-04-20 11:32:01 -0400

    Revert "ExprTinyText - Merge old Text.java"
    This reverts commit 38c726a5f851db1c32374a450903726a0f0ec9ee.

--- src/main/java/lol/aabss/skuishy/elements/general/expressions/ExprTinyText.java
+++ src/main/java/lol/aabss/skuishy/elements/general/expressions/ExprTinyText.java
@@ -12,12 +12,11 @@ import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.util.LiteralUtils;
 import ch.njol.util.Kleenean;
+import lol.aabss.skuishy.other.Text;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
-import java.util.Arrays;
-import java.util.List;
 
 @Name("Other - Tiny Text")
 @Description("Returns a text in tiny caps.")
@@ -27,37 +26,27 @@ import java.util.List;
 @Since("2.7")
 public class ExprTinyText extends SimpleExpression<String> {
 
-    private static final List<String> normal_char_set = Arrays.stream("abcdefghijklmnopqrstuvwxyz".split("")).toList();
-    private static final List<String> tiny_char_set = Arrays.stream("ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ".split("")).toList();
-    private static final List<String> super_tiny_char_set = Arrays.stream("ᵃᵇᶜᵈᵉᶠᵍʰᶦʲᵏˡᵐⁿᵒᵖᵠʳˢᵗᵘᵛʷˣʸᶻ".split("")).toList();
-
     static {
         Skript.registerExpression(ExprTinyText.class, String.class, ExpressionType.COMBINED,
                 "[:super] tiny [(text|caps)] %objects%",
-                "%objects% (in|as) [:super] tiny (text|caps)"
+                "%objects% in [:super] tiny (text|caps)"
         );
     }
 
-    private Expression<Object> objects;
-    private boolean isSuperTiny;
-
-    @Override
-    public boolean init(Expression<?>[] exprs, int i, @NotNull Kleenean kleenean, SkriptParser.ParseResult parseResult) {
-        objects = LiteralUtils.defendExpression(exprs[0]);
-        this.isSuperTiny = parseResult.hasTag("super");
-        return LiteralUtils.canInitSafely(objects);
-    }
+    private Expression<Object> text;
+    private boolean superTiny;
 
     @Override
     protected String @NotNull [] get(@NotNull Event event) {
-        return objects.stream(event)
+        return text.stream(event)
                 .map(Classes::toString)
-                .map(this::tinyText).toArray(String[]::new);
+                .map(string -> Text.tinyText(string, superTiny))
+                .toArray(String[]::new);
     }
 
     @Override
     public boolean isSingle() {
-        return objects.isSingle();
+        return text.isSingle();
     }
 
     @Override
@@ -66,23 +55,14 @@ public class ExprTinyText extends SimpleExpression<String> {
     }
 
     @Override
-    public @NotNull String toString(@Nullable Event event, boolean debug) {
-        return this.objects.toString(event, debug) + " as " + (isSuperTiny ? "super " : "") + "tiny text";
+    public @NotNull String toString(@Nullable Event event, boolean b) {
+        return "tiny text";
     }
 
-    public String tinyText(String string){
-        StringBuilder tinyText = new StringBuilder();
-        List<String> char_set = isSuperTiny ? super_tiny_char_set : tiny_char_set;
-        boolean isColorCode = false; // Preset the isColorCode to false
-        for (String str : string.split("")) {
-            int index = normal_char_set.indexOf(str);
-            if (!isColorCode && index != -1) { // Checks if string is not a color code and index was founds
-                str = char_set.get(index);
-            }
-            isColorCode = str.equals("§"); // Ensures the next loop uses a normal character
-            tinyText.append(str);
-        }
-        return tinyText.toString();
+    @Override
+    public boolean init(Expression<?>[] exprs, int i, @NotNull Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+        text = LiteralUtils.defendExpression(exprs[0]);
+        superTiny = parseResult.hasTag("super");
+        return LiteralUtils.canInitSafely(text);
     }
-
 }
